c319dbb033cc91ba3de6835792b52947dc418507
arburhan <buthan190629@outlook.com>
2022-05-02 17:08:09 +0600

    navbar added

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Container, Nav } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Inventory Management</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#login">Log in</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
