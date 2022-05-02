8723ad5f131643fbc29c37acf76d36e8274308c9
arburhan <buthan190629@outlook.com>
2022-05-02 22:42:24 +0600

    toast added on reset mail

--- src/Components/Pages/ResetPassword/ResetPassword.js
+++ src/Components/Pages/ResetPassword/ResetPassword.js
@@ -4,6 +4,8 @@ import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const ResetPassword = () => {
@@ -11,20 +13,21 @@ const ResetPassword = () => {
     const navigate = useNavigate();
     let errorElement;
     const [sendPasswordResetEmail, sending, errorPassReset] = useSendPasswordResetEmail(auth);
-    const handleReset = e => {
+    const handleReset = async e => {
         e.preventDefault();
         const email = emailRef.current.value;
         sendPasswordResetEmail(email);
+        toast('reset mail sent. please check spam or inbox');
     }
-    if (sending) {
+    /* if (sending) {
         return <Loading></Loading>
-    }
+    } */
     if (errorPassReset) {
         errorElement = <p className='text-danger'>Error: {errorPassReset?.message}</p>
     }
     return (
         <div className='container mx-auto w-75 py-5'>
-            <h3 className='text-center py-4'>Please enter your email for reset password</h3>
+            <h3 className='text-center py-4'>Enter your email for reset password</h3>
             <Form onSubmit={handleReset} className='text-center'>
                 <div className='' >
                     <label className='d-block text-start ps-2' htmlFor="email">Email</label>
@@ -36,7 +39,7 @@ const ResetPassword = () => {
                     <button type='submit' className='btn submit-btn'>Send Reset Mail</button>
                 </div>
             </Form>
-
+            <ToastContainer />
         </div>
     );
 };
