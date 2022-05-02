1857826fe8cc4d6455398635294154cf2b4278ab
arburhan <buthan190629@outlook.com>
2022-05-02 17:52:58 +0600

    add route

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Container, Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Inventory Management</Navbar.Brand>
+                    <Navbar.Brand as={CustomLink} to='/' href="#home">Inventory Management</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#login">Log in</Nav.Link>
-                            <Nav.Link href="#blogs">Blogs</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/home' href="#home">Home</Nav.Link>
+                            <Nav.Link as={CustomLink} to='login' href="#login">Log in</Nav.Link>
+                            <Nav.Link as={CustomLink} to='/blogs' href="#blogs">Blogs</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -21,5 +22,22 @@ const Header = () => {
         </>
     );
 };
+
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{ textDecoration: match ? "underline white" : "none" }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+        </div>
+    );
+}
 
 export default Header;
