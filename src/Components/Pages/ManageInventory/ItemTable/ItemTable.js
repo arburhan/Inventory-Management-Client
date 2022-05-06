0cd9d0207feb32ee949ca8c57113f1b16d9bf229
arburhan <buthan190629@outlook.com>
2022-05-06 12:06:52 +0600

    style add manage all items

--- src/Components/Pages/ManageInventory/ItemTable/ItemTable.js
+++ src/Components/Pages/ManageInventory/ItemTable/ItemTable.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table'
+import './ItemTable.css';
 
-const ItemTable = () => {
+const ItemTable = ({ item }) => {
+    const { _id, name } = item;
     return (
-        <div>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table>
-
-        </div>
+        <tr>
+            <td>{_id}</td>
+            <td>{name}</td>
+            <td className='text-center'><button className='inventory-up manage-btn'>Update</button></td>
+            <td className='text-center'><button className='delete-btn manage-btn' >Delete</button></td>
+        </tr>
     );
 };
 
--- src/Components/Pages/ManageInventory/ManageInventory.js
+++ src/Components/Pages/ManageInventory/ManageInventory.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import useInventory from '../../Hooks/useInventory';
 import ItemTable from './ItemTable/ItemTable';
 
 const ManageInventory = () => {
+    const [items] = useInventory();
     return (
-        <div>
-            <h2>Manage Inventory</h2>
-            <ItemTable></ItemTable>
-
+        <div className='container'>
+            <h2 className='text-center'>Manage Inventory</h2>
+            <Table responsive striped bordered>
+                <thead>
+                    <tr>
+                        <th>Item id</th>
+                        <th>Name</th>
+                        <th>Update Product</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        items.map(item => <ItemTable key={item._id} item={item}></ItemTable>)
+                    }
+                </tbody>
+            </Table>
         </div>
     );
 };
