0129d718fadda7d06f61fbf3a4dd808ed3e4c75c
arburhan <buthan190629@outlook.com>
2022-05-06 16:05:00 +0600

    add item successfully

--- src/Components/Pages/AddItem/AddItem.js
+++ src/Components/Pages/AddItem/AddItem.js
@@ -4,22 +4,36 @@ import './AddItem.css';
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        const url = ` https://sheltered-citadel-07680.herokuapp.com/inventory`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+            })
+
+    };
     return (
         <div style={{ backgroundColor: '#2b2d42' }} className='container-fluid text-white pb-3'>
             <h2 className='text-center pt-4'>Add New Product</h2>
             <div className='w-75 mx-auto pt-2'>
                 <form onSubmit={handleSubmit(onSubmit)} className='text-center'>
                     <label className='mt-2 d-block text-start' htmlFor="productName">Product Name</label>
-                    <input placeholder='Product Name' {...register("productName")} required />
+                    <input placeholder='Product Name' {...register("name")} required />
                     <label className='mt-2 d-block text-start' htmlFor="suplierName">Suplier Name</label>
-                    <input placeholder='Suplier Name' {...register("suplierName")} required />
+                    <input placeholder='Suplier Name' {...register("suplier")} required />
                     <label className='mt-2 d-block text-start' htmlFor="price">Price</label>
                     <input placeholder='Price' type="number" {...register("price")} required />
                     <label className='mt-2 d-block text-start' htmlFor="quantity">Quantity</label>
                     <input placeholder='Quantity' type="number" {...register("quantity")} required />
                     <label className='mt-2 d-block text-start' htmlFor="imageLink">Image Link</label>
-                    <input placeholder='Image Link' {...register("imageLink")} required />
+                    <input placeholder='Image Link' {...register("image")} required />
                     <label className='d-block text-start' htmlFor="description">Description</label>
                     <textarea className='rounded textarea-space' name="description" id="" {...register("description")}></textarea>
                     <div className='py-2'>
