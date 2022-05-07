b5ed7addbe5f7297613f5995bd3fd3c04b6928f6
arburhan <buthan190629@outlook.com>
2022-05-08 00:43:43 +0600

    weekly update section added

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import AllCategory from '../AllCategory/AllCategory';
 import InventoryItems from '../InventoryItems/InventoryItems';
+import StockUpdates from '../StockUpdates/StockUpdates';
 import Banner from './Banner/Banner';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <InventoryItems></InventoryItems>
             <AllCategory></AllCategory>
+            <StockUpdates></StockUpdates>
             <Footer></Footer>
         </div>
     );
---
+++ src/Components/Pages/StockUpdates/StockUpdates.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './StockUpdates.css';
+
+const StockUpdates = () => {
+    return (
+        <div className='text-center'>
+            <h2 className=''>Weekly Updates</h2>
+            <div className='parent-stock fw-bold my-5'>
+                <div className='category-singleDiv category-responsive category-textSpace'>
+                    <h1>2M</h1>
+                    <h4>We Stock</h4>
+                </div>
+                <div className='category-singleDiv category-responsive category-textSpace'>
+                    <h1>1.8M</h1>
+                    <h4>We Sold</h4>
+                </div>
+                <div className='category-singleDiv category-textSpace'>
+                    <h1>30K</h1>
+                    <h4>Customer</h4>
+                </div>
+                <div className=' category-textSpace'>
+                    <h1>2K</h1>
+                    <h4>Employee</h4>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default StockUpdates;
