1a7d9e1b5da29b5e4aaf5fabdc26c630f4c3deb8
arburhan <buthan190629@outlook.com>
2022-05-07 16:32:17 +0600

    my items data load diffirent users

--- src/Components/Pages/MyItems/MyItems.js
+++ src/Components/Pages/MyItems/MyItems.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyItems = () => {
+    const [items, setItems] = useState([]);
+    const [user] = useAuthState(auth);
+    const email = user?.email;
+    useEffect(() => {
+        const filterItem = async () => {
+            const url = ` https://sheltered-citadel-07680.herokuapp.com/myitems?email=${email}`;
+            const { data } = await axios.get(url);
+            setItems(data);
+        }
+        filterItem();
+    }, [user])
+
     return (
         <div>
-            <h2>Your Items</h2>
+            <h2>Your Items {items.length} </h2>
 
         </div>
     );
