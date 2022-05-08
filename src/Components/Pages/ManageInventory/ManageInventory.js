aa2540f5d121251901c6ffd6a89c661b7dcf94d3
arburhan <buthan190629@outlook.com>
2022-05-08 09:55:55 +0600

    add button add on manage page

--- src/Components/Pages/ManageInventory/ManageInventory.js
+++ src/Components/Pages/ManageInventory/ManageInventory.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import useInventory from '../../Hooks/useInventory';
 import ItemTable from './ItemTable/ItemTable';
+import './ManageInventory.css';
 
 const ManageInventory = () => {
+    const navigate = useNavigate();
     const [items] = useInventory();
+    const addNewItems = e => {
+        navigate('/additem');
+
+
+    }
     return (
         <div className='container py-2'>
             <h2 className='text-center py-4'>Manage All Items</h2>
@@ -23,6 +31,9 @@ const ManageInventory = () => {
                     }
                 </tbody>
             </Table>
+            <div className="text-center py-4">
+                <button onClick={addNewItems} className="see-btn addNew-btn">Add New Items</button>
+            </div>
         </div>
     );
 };
