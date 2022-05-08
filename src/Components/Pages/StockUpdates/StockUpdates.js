2c9ad32b5b7448be56bd6af8e9793d489d27e5f4
arburhan <buthan190629@outlook.com>
2022-05-08 11:36:58 +0600

    space add on week update section

--- src/Components/Pages/StockUpdates/StockUpdates.js
+++ src/Components/Pages/StockUpdates/StockUpdates.js
@@ -4,25 +4,27 @@ import './StockUpdates.css';
 const StockUpdates = () => {
     return (
         <div className='text-center'>
-            <h2 className=''>Weekly Updates</h2>
-            <div className='parent-stock fw-bold my-5'>
-                <div className='category-singleDiv category-responsive category-textSpace'>
-                    <h1>2M</h1>
-                    <h4>We Stock</h4>
-                </div>
-                <div className='category-singleDiv category-responsive category-textSpace'>
-                    <h1>1.8M</h1>
-                    <h4>We Sold</h4>
-                </div>
-                <div className='category-singleDiv category-textSpace'>
-                    <h1>30K</h1>
-                    <h4>Customer</h4>
-                </div>
-                <div className=' category-textSpace'>
-                    <h1>2K</h1>
-                    <h4>Employee</h4>
-                </div>
+            <div className='parent-stock fw-bold pt-4'>
+                <h2 className=''>Weekly Updates</h2>
+                <div className='parent-stock-div'>
+                    <div className='category-singleDiv category-responsive category-textSpace'>
+                        <h1>2M</h1>
+                        <h4>We Stock</h4>
+                    </div>
+                    <div className='category-singleDiv category-responsive category-textSpace'>
+                        <h1>1.8M</h1>
+                        <h4>We Sold</h4>
+                    </div>
+                    <div className='category-singleDiv category-textSpace'>
+                        <h1>30K</h1>
+                        <h4>Customer</h4>
+                    </div>
+                    <div className=' category-textSpace'>
+                        <h1>2K</h1>
+                        <h4>Employee</h4>
+                    </div>
 
+                </div>
             </div>
         </div>
     );
