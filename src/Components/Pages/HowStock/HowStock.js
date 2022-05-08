9d005f1e64ba00213c958a5ded9a576f18c40216
arburhan <buthan190629@outlook.com>
2022-05-08 11:44:44 +0600

    url set

--- src/Components/Pages/HowStock/HowStock.js
+++ src/Components/Pages/HowStock/HowStock.js
@@ -3,6 +3,9 @@ import stockImage from './stockStep.webp';
 import './HowStock.css';
 
 const HowStock = () => {
+    const howStockHandle = () => {
+        window.open('https://www.wikihow.com/Stock-Grocery-Shelves');
+    }
     return (
         <div className='py-5'>
             <h2 className='text-center'>How We Stock !</h2>
@@ -10,7 +13,7 @@ const HowStock = () => {
                 <div className='px-2 px-md-4 text-center text-md-start'>
                     <h5 className='stock-text'>In our gorcery we stock the product in several steps. First of all we supply fresh, formalin free, daily produced products to the farmers. Then we clean them better by machine and pack the packets in a healthy way and bring them to gorcery. However we try not to stock excess products so that the product becomes stale.</h5>
                     <div className=''>
-                        <button className='monitor-btn'>Monitor Steps</button>
+                        <button onClick={howStockHandle} className='monitor-btn'>Monitor Steps</button>
                     </div>
                 </div>
                 <div className='img-div-stock'>
