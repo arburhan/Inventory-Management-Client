6244afe601cccdec1ab426d20f9fca0c14160943
arburhan <buthan190629@outlook.com>
2022-05-08 12:58:54 +0600

    style add on feature cards

--- src/Components/Pages/Features/Features.js
+++ src/Components/Pages/Features/Features.js
@@ -5,13 +5,15 @@ import supportImage from './Images/support.png';
 import securityImage from './Images/security.png';
 const Features = () => {
     return (
-        <div className='text-center'>
-            <h2>Our Features</h2>
-            <p>We offer various types of services to make our clients happy. Here are some of them.</p>
+        <div className='text-center my-5'>
+            <div className='py-3'>
+                <h2>Our Features</h2>
+                <p>We offer various types of services to make our clients happy. Here are some of them.</p>
+            </div>
             <div className='parent-feature-div'>
                 <div className='service'>
                     <img className='img-fluid' src={shippingImage} alt="" />
-                    <h3>Super Fast Delivery</h3>
+                    <h3>Fast Delivery</h3>
                     <p>We are the only one who can guarantee you rocket speed shipping. It is too late for your order but our product is not too late to reach your door. We do shipping with aliens (lies).</p>
                 </div>
                 <div className='service'>
