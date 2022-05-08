18933048e2e053ef49071dfdf47ae9c3d59ae52b
arburhan <buthan190629@outlook.com>
2022-05-08 14:33:49 +0600

    add new style on footer

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
-import avatarImage from '../../../Images/user.png';
+import { useNavigate } from 'react-router-dom';
 import './Footer.css';
-const Footer = () => {
-    const [user, loading, error] = useAuthState(auth);
 
+const Footer = () => {
+    const navigate = useNavigate();
+    const redirectHome = () => {
+        navigate('/home');
+    }
+    const redireBlogs = () => {
+        navigate('/blogs');
+    }
+    const redirectLogin = () => {
+        navigate('/login');
+    }
+    const redirectAbout = () => {
+        navigate('/about');
+    }
     return (
         <div className='bg-dark text-light'>
             <div>
-                <h3>Account</h3>
-                {
-                    !user ? <button className='footer-btn'>Login</button> : user.photoURL == null ? <img className='img-fluid top-img dropdown-toggle' src={avatarImage} alt="" /> : <img className='img-fluid top-img ' src={user.photoURL} alt="" />
-                }
+                <h5>Useful Links</h5>
+                <div>
+                    <button onClick={redirectHome} className='footer-btn'>Home</button>
+                </div>
+                <div>
+                    <button onClick={redireBlogs} className='footer-btn'>Blogs</button>
+                </div>
+                <div>
+                    <button onClick={redirectLogin} className='footer-btn'>Login</button>
+                </div>
+                <div>
+                    <button onClick={redirectAbout} className='footer-btn'>About</button>
+                </div>
             </div>
 
         </div>
